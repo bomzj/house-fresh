a448916a81c9f13d2bba7a53e61c8ae692e8469a
Maksim Shamihulau <maksim@brainjocks.com>
2018-08-07 00:30:38 +0300

    make all *.css to be *.scss and bundle them in one main.css

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,4 @@
+import '../scss/main.scss';
 import $ from './jquery';
 import boostrap from './bootstrap.min';
 import './jquery.easing.min';
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,12 @@
 const webpack = require('webpack'); //to access built-in plugins
 const path = require('path');
 const glob = require('glob');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
     mode: 'production',
-    entry: "./static/js/main.js",//glob.sync('./themes/hugo-creative-theme/static/js/*.js'),
+	//mode: 'development',
+    entry: "./src/js/main.js",//glob.sync('./themes/hugo-creative-theme/static/js/*.js'),
     output: {
         path: path.resolve(__dirname, './static/js/'),
         filename: 'bundle.js'
@@ -14,25 +16,45 @@ module.exports = {
     // },
     module: {
         rules: [
-            // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
-            // { test: /\.tsx?$/, loader: 'ts-loader' },
-            // { test: /\.js$/, loader: 'ts-loader' },
-            // {
-                // test: /\.s?css$/, loaders: ['style-loader', 'css-loader'],
-                // //options: {
-                // //    modules: true
-                // //}
-            // }
-        ]
-    },
+		// { 
+			// test: /\.js$/,
+			// use: {
+			  // loader: "babel-loader",
+			  // options: { presets: ["es2015"] }
+			// }
+		// },
+		{
+			test: /\.scss$/,
+			use: [
+                MiniCssExtractPlugin.loader,
+				{
+					loader: 'css-loader',
+					options: {
+						//if you are having trouble with urls not resolving add this setting.
+						//see https://github.com/webpack-contrib/css-loader#url
+						url: false,
+						minimize: true,
+						//sourceMap: true
+					}
+				}, 
+				{
+					loader: 'sass-loader',
+					options: {
+						//sourceMap: true
+					}
+				}
+            ]
+		}]
+	},
     plugins: [
-       // new webpack.optimize.UglifyJsPlugin(),
-        //new SingleEntryPlugin(__dirname, "./app", "bundle"),
-     //   new HtmlWebpackPlugin({ template: './src/index.html' })
-		 new webpack.ProvidePlugin({   
-		 jquery: './jquery',
-		 $: './jquery',
-		 jQuery: './jquery'
+		// For bootstrap.js during compilation
+		new webpack.ProvidePlugin({   
+			jquery: './jquery',
+			$: './jquery',
+			jQuery: './jquery'
+		}),
+		new MiniCssExtractPlugin({
+		  filename: "../css/[name].css"
 		})
     ]
 };
