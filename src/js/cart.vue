4f8fe9f6bbf552b87aa87492e6a7a499c22dbbe5
Maksim Shamihulau <maksim@brainjocks.com>
2018-08-26 17:41:33 +0300

    toggle count icon visibility in the cart button

--- src/js/cart.vue
+++ src/js/cart.vue
@@ -2,7 +2,7 @@
 <div>
 <span class="navbar-cart">
 	<a href="/#cart-modal" class="btn-open-cart" data-toggle="modal"><i class="fa fa-shopping-cart fa-2x"> </i>
-		<span class="cart-items-count">{{ items.length }}</span>
+		<span class="cart-items-count" v-if="items.length">{{ items.length }}</span>
 	 </a>
 </span>
 
