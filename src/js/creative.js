d4976cc4d18f611d2f13a708c7a26f09b14bf1c7
Alena Shamihulava <alena.shamihulava@gmail.com>
2019-03-19 11:36:40 +0300

    add css for responsive text size, delete js.

--- src/js/creative.js
+++ src/js/creative.js
@@ -27,22 +27,6 @@
         $('.navbar-toggle:visible').click();
     });
 
-    // Fit Text Plugin for Main Header
-    $("h1").fitText(
-        1.2, {
-            minFontSize: '35px',
-            maxFontSize: '65px'
-        }
-    );
-	
-	//Fit Text for Services section
-	$("h2.service-title").fitText(
-        1.2, {
-            minFontSize: '35px',
-            maxFontSize: '65px'
-        }
-    );
-
     // Offset for Main Navigation
     $('#mainNav').affix({
         offset: {
